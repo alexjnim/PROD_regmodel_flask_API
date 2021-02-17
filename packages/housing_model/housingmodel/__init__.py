462ae93c3b377b1581baed66355927b7a619c6d4
Alex Nim <alex.nim@ibm.com>
2021-02-17 11:29:28 +0000

    adding comments for logger

--- packages/housing_model/housingmodel/__init__.py
+++ packages/housing_model/housingmodel/__init__.py
@@ -8,6 +8,9 @@ VERSION_PATH = config.PACKAGE_ROOT / "VERSION"
 
 # Configure logger for use in package
 logger = logging.getLogger(__name__)
+
+# set the level to DEBUG so all messages will be printed in the console
+# the default level is usually WARNING
 logger.setLevel(logging.DEBUG)
 logger.addHandler(logging_config.get_console_handler())
 logger.propagate = False
