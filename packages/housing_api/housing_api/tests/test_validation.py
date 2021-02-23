9ff238e9c87081f630cdafb3b29217f0ad5c6663
Alex Nim <alex.nim@ibm.com>
2021-02-23 16:02:52 +0000

    uncommenting out the validation test

--- packages/housing_api/housing_api/tests/test_validation.py
+++ packages/housing_api/housing_api/tests/test_validation.py
@@ -33,46 +33,46 @@ def test_prediction_endpoint_validation_200(flask_test_client):
 # parameterizationa allows us to try many combinations of data
 # within the same test, see the pytest docs for details:
 # https://docs.pytest.org/en/latest/parametrize.html
-# @pytest.mark.parametrize(
-#     "field, field_value, index, expected_error",
-#     (
-#         (
-#             "longitude",
-#             "hello",  # expected float
-#             0,
-#             {"0": {"longitude": ["Not a valid number."]}},
-#         ),
-#         (
-#             "ocean_proximity",  # model feature
-#             12.0,  # expected string
-#             45,
-#             {"45": {"ocean_proximity": ["Not a valid string."]}},
-#         ),
-#         (
-#             "households",
-#             np.nan,  # nan not allowed
-#             34,
-#             {"34": {"households": ["Field may not be null."]}},
-#         ),
-#         ("total_bedrooms", "", 2, {"2": {"total_bedrooms": ["Not a valid number."]}}),
-#     ),
-# )
-# def test_prediction_validation(
-#     field, field_value, index, expected_error, flask_test_client
-# ):
-#     # Given
-#     # In this test, inputs are changed to incorrect values to check the validation.
-#     test_data = load_dataset(file_name=model_config.TESTING_DATA_FILE)
-#     test_data.loc[index, field] = field_value
-#     post_json = test_data.to_json(orient="records")
+@pytest.mark.parametrize(
+    "field, field_value, index, expected_error",
+    (
+        (
+            "longitude",
+            "hello",  # expected float
+            0,
+            {"0": {"longitude": ["Not a valid number."]}},
+        ),
+        (
+            "ocean_proximity",  # model feature
+            12.0,  # expected string
+            45,
+            {"45": {"ocean_proximity": ["Not a valid string."]}},
+        ),
+        (
+            "households",
+            np.nan,  # nan not allowed
+            34,
+            {"34": {"households": ["Field may not be null."]}},
+        ),
+        ("total_bedrooms", "", 2, {"2": {"total_bedrooms": ["Not a valid number."]}}),
+    ),
+)
+def test_prediction_validation(
+    field, field_value, index, expected_error, flask_test_client
+):
+    # Given
+    # In this test, inputs are changed to incorrect values to check the validation.
+    test_data = load_dataset(file_name=model_config.TESTING_DATA_FILE)
+    test_data.loc[index, field] = field_value
+    post_json = test_data.to_json(orient="records")
 
-#     # When
-#     response = flask_test_client.post(
-#         "/v1/predict/regression", json=json.loads(post_json)
-#     )
+    # When
+    response = flask_test_client.post(
+        "/v1/predict/regression", json=json.loads(post_json)
+    )
 
-#     # Then
-#     assert response.status_code == 200
-#     response_json = json.loads(response.data)
-#     print(response_json)
-#     assert response_json["errors"] == expected_error
+    # Then
+    assert response.status_code == 200
+    response_json = json.loads(response.data)
+    print(response_json)
+    assert response_json["errors"] == expected_error
